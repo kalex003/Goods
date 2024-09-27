e50adc8e9df719fc3dc2fd600d43fa8b835aa13f
Курочкин Александр <kalex160103@gmail.com>
2024-09-27 21:46:50 +0300

    fix

--- internal/domain/models/GoodFullInfo.go
+++ internal/domain/models/GoodFullInfo.go
@@ -1,5 +1,11 @@
 package models
 
+//поч импорт пакета целиком не работает?
+import (
+	goodsv1 "github.com/kalex003/Goods_Proto/gen/go/goods"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
 import "time"
 
 type GoodFullInfo struct {
@@ -18,7 +24,11 @@ type GoodFullInfo struct {
 	IsDel        bool      `db:"is_del"`
 }
 
-func NewFullInfo(GoodsId int64, PlaceId int64, SkuId int64, WbstickerId int64, Barcode string, StateId string, ChEmployeeId int64, OfficeId int64, WhId int64, TareId int64, TareType string, ChDt time.Time, IsDel bool) *GoodFullInfo {
+type GoodsFullInfo struct {
+	GoodsFullInfo []GoodFullInfo
+}
+
+func NewFullInfo(GoodsId int64, PlaceId int64, SkuId *int64, WbstickerId *int64, Barcode *string, StateId *string, ChEmployeeId int64, OfficeId *int64, WhId *int64, TareId *int64, TareType *string, ChDt time.Time, IsDel bool) *GoodFullInfo {
 	return &GoodFullInfo{
 		GoodsId:      GoodsId,
 		PlaceId:      PlaceId,
@@ -33,5 +43,66 @@ func NewFullInfo(GoodsId int64, PlaceId int64, SkuId int64, WbstickerId int64, B
 		TareType:     TareType,
 		ChDt:         ChDt,
 		IsDel:        IsDel,
+	}
+}
+
+// Преобразование одной структуры GoodFullInfo в OneGetResponse
+func ConvertGoodFullInfoToOneGetResponse(good GoodFullInfo) *goodsv1.OneGetResponse {
+	return &goodsv1.OneGetResponse{
+		GoodsId:      good.GoodsId,
+		PlaceId:      good.PlaceId,
+		SkuId:        good.SkuId,
+		WbstickerId:  good.WbstickerId,
+		Barcode:      good.Barcode,
+		StateId:      good.StateId,
+		ChEmployeeId: good.ChEmployeeId,
+		OfficeId:     good.OfficeId,
+		WhId:         good.WhId,
+		TareId:       good.TareId,
+		TareType:     good.TareType,
+		ChDt:         timestamppb.New(good.ChDt),
+		IsDel:        good.IsDel,
+	}
+}
+
+// Преобразование массива структур GoodFullInfo в массив GetResponse
+func ConvertGoodsFullInfoToGetResponse(goods GoodsFullInfo) *goodsv1.GetResponse { //пока буду указатель отдавать
+	var responses []*goodsv1.OneGetResponse
+	for _, good := range goods.GoodsFullInfo {
+		response := ConvertGoodFullInfoToOneGetResponse(good)
+		responses = append(responses, response)
+	}
+
+	return &goodsv1.GetResponse{
+		Structs: responses,
+	}
+}
+
+type GoodInsertAnswer struct {
+	GoodsId int64     `db:"goods_id"`
+	ChDt    time.Time `db:"ch_dt"`
+}
+
+type GoodsInsertAnswer struct {
+	GoodsInsertAnswer []GoodInsertAnswer
+}
+
+// Преобразование одной структуры GoodInsertAnswer в OneInsertResponse
+func ConvertGoodInsertAnswerToOneInsertResponse(answer GoodInsertAnswer) *goodsv1.OneInsertResponse {
+	return &goodsv1.OneInsertResponse{
+		GoodsId: answer.GoodsId,
+		ChDt:    timestamppb.New(answer.ChDt),
+	}
+}
+
+func ConvertGoodsInsertAnswerToInsertResponse(answers GoodsInsertAnswer) *goodsv1.InsertResponse { //пока буду указатель отдавать
+	var responses []*goodsv1.OneInsertResponse
+	for _, answer := range answers.GoodsInsertAnswer {
+		response := ConvertGoodInsertAnswerToOneInsertResponse(answer)
+		responses = append(responses, response)
+	}
+
+	return &goodsv1.InsertResponse{
+		Structs: responses,
 	}
 }
